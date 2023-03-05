6cdfc260ccd925708a32b847737605365b2cc7a4
StouryMehdi <mehdistoury@gmail.com>
2023-03-05 18:30:12 +0100

    need to add functions

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import Wrapper from "./components/Wrapper";
 import Screen from "./components/Screen";
 import ButtonBox from "./components/ButtonBox";
 import Button from "./components/Button";
+import React,{useState} from "react";
+
 
 const btnValues = [
-  ["C", "+-", "%", "/"],
+  ["C", "+/-", "%", "/"],
   [7, 8, 9, "X"],
   [4, 5, 6, "-"],
   [1, 2, 3, "+"],
@@ -12,9 +14,15 @@ const btnValues = [
 ];
 
 const App = () => {
+let [calc,setCalc] = useState({
+  sign:"",
+  num:0,
+  res:0,
+})
+
   return (
     <Wrapper>
-      <Screen value="0" />
+      <Screen value={calc.num ? calc.num : calc.res} />
       <ButtonBox>
         {
           btnValues.flat().map((btn, i) => {
@@ -23,9 +31,19 @@ const App = () => {
                 key={i}
                 className={btn === "=" ? "equals" : ""}
                 value={btn}
-                onClick={() => {
-                  console.log(`${btn} clicked!`);
-                }}
+                onClick={
+                  btn === "C"
+                  ? resetClickHandler
+                  : btn === "+/-"
+                  ? invertClickHandler
+                  : btn === "="
+                  ? equalsClickHandler
+                  : btn === "/" || btn ==="X" || btn === "-" || btn === "+"
+                  ? signClickHandler
+                  : btn === "."
+                  ? commaClickHandler
+                  : numClickHandler
+                }
               />
             );
           })
