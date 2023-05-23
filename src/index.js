f7baa0ba8eb78020c9fdcca4cb839824c2deedbc
StouryMehdi <mehdistoury@gmail.com>
2023-05-23 23:04:43 +0200

    Calculator Complet

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import { createRoot } from 'react-dom';
 
-import App from "./App";
-import "./index.css";
+import App from './App';
+import './index.css';
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
